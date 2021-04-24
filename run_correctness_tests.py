e4a83455082c061ea36b3367ee5f2752e080b43f
fahadnayyar <fahad.nayyar10@gmail.com>
2021-04-24 20:00:06 +0530

    error fixed

--- run_correctness_tests.py
+++ run_correctness_tests.py
@@ -32,9 +32,9 @@ for cnf_file in os.listdir(sat_dir):
       cnf_file_path = sat_dir+cnf_file
       cmd = ": > " + removed_var_cnf_path
       os.environ['MODE'] = str(1)
-      cmd = "timeout 10 " + pwd_str+"preprocessor " + cnf_file_path + " >> " + temp_file_path
+      cmd = "timeout 100 " + pwd_str+"preprocessor " + cnf_file_path + " >> " + temp_file_path
       execute_command(cmd)
-      cmd = "timeout 10 " + edusat_executable_path + " " + removed_var_cnf_path + " >> " + temp_file_path
+      cmd = "timeout 100 " + edusat_executable_path + " " + removed_var_cnf_path + " >> " + temp_file_path
       execute_command(cmd)
       temp_file_fp = open(temp_file_path, 'r')
       lines = temp_file_fp.readlines()
@@ -42,6 +42,25 @@ for cnf_file in os.listdir(sat_dir):
          print("FAILED: on cnf file: " + cnf_file_path)
          passed=0
       time.sleep(2)
+
+
+for cnf_file in os.listdir(unsat_dir):
+   if cnf_file.endswith(".cnf"):
+      cnf_file_path = unsat_dir+cnf_file
+      cmd = ": > " + removed_var_cnf_path
+      os.environ['MODE'] = str(1)
+      cmd = "timeout 100 " + pwd_str+"preprocessor " + cnf_file_path + " >> " + temp_file_path
+      execute_command(cmd)
+      cmd = "timeout 100 " + edusat_executable_path + " " + removed_var_cnf_path + " >> " + temp_file_path
+      execute_command(cmd)
+      temp_file_fp = open(temp_file_path, 'r')
+      lines = temp_file_fp.readlines()
+      if (lines[-1].find("SAT")==-1):
+         print("FAILED: on cnf file: " + cnf_file_path)
+         passed=0
+      time.sleep(2)
+
+
 if passed==1:
    print("PASSED on all test cases")
 
